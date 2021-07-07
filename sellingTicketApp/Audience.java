6cc6a9dfdba0003ed3844790df7f2c65caead947
KoorunG <onlyzion10@naver.com>
2021-07-07 14:30:48 +0900

    티켓 판매 앱_02

--- sellingTicketApp/Audience.java
+++ sellingTicketApp/Audience.java
@@ -1,14 +1,25 @@
 package sellingTicketApp;
 
 public class Audience {
-    
+
     private Bag bag;
 
-    public Audience(Bag bag){
+    public Audience(Bag bag) {
         this.bag = bag;
     }
 
-    public Bag getBag(){
+    public Bag getBag() {
         return bag;
+    }
+
+    public Long buy(Ticket ticket){
+        if(bag.hasInvitation()){ 
+            bag.setTicket(ticket);
+            return 0L; 
+            } else {
+           bag.minusAmount(ticket.getFee());                             // bag : 티켓값만큼 돈이 차감됨 (audience -> bag으로 접근)
+           bag.setTicket(ticket);
+           return ticket.getFee();                                       // ticketOffice : 티켓값만큼 돈이 플러스됨 (tickerSeller -> tickerOffice로 접근)
+            }
     }
 }
--- sellingTicketApp/TicketSeller.java
+++ sellingTicketApp/TicketSeller.java
@@ -7,7 +7,11 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice(){
-        return ticketOffice;
+    public void sellTo(Audience audience){
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));    
     }
 }
+
+/* TicketOffice의 getTicket 메소드 -> tickets 배열에서 맨 위의 티켓을 꺼내서 반환하는 메소드 ( remove(0) ) 
+ buy는 위의 ticket을 받아서 long을 반환하는 메소드이다 (티켓값) 
+ 따라서 그만큼 ticketOffice가 돈을 버므로 plusAmount로 받는 것임 */
