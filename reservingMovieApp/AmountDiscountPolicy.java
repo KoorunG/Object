94a02c19f615c0104a8184fa43d25e12a467fb75
KoorunG <onlyzion10@naver.com>
2021-07-08 17:52:37 +0900

    금액이 0원일 경우를 대비해 인터페이스를 추가한 경우

--- reservingMovieApp/AmountDiscountPolicy.java
+++ reservingMovieApp/AmountDiscountPolicy.java
@@ -1,17 +1,16 @@
 package reservingMovieApp;
 
-public class AmountDiscountPolicy extends DiscountPolicy {
+public class AmountDiscountPolicy extends DefaultDiscountPolicy {
 
     private Money discountAmount;
 
     public AmountDiscountPolicy(Money discountAmount, DiscountCondition ... conditions) {       // 파라미터 순서에 따라 오류가 발생한다... ?????
-                                                                                                // super(conditions)는 맨 위에 와야한다, 따라서 conditions 인자를 받는 파라미터 또한 앞에 와야한다
-        super(conditions);
+        super(conditions);                                                                                    // super(conditions)는 맨 위에 와야한다, 따라서 conditions 인자를 받는 파라미터 또한 앞에 와야한다
         this.discountAmount = discountAmount;
     }
 
     @Override
-    protected Money getDiscountAmount(Screening screening) {
+    public Money getDiscountAmount(Screening screening) {
         return discountAmount;
     }
     
--- reservingMovieApp/PercentDiscountPolicy.java
+++ reservingMovieApp/PercentDiscountPolicy.java
@@ -1,18 +1,16 @@
 package reservingMovieApp;
 
-public class PercentDiscountPolicy extends DiscountPolicy {
+public class PercentDiscountPolicy extends DefaultDiscountPolicy {
 
     private double percent;
-
     
     public PercentDiscountPolicy(double percent,DiscountCondition ... conditions) {
-        super(conditions);
+        super(conditions); 
         this.percent = percent;
     }
 
-
     @Override
-    protected Money getDiscountAmount(Screening screening) {
+    public Money getDiscountAmount(Screening screening) {
         
         return screening.getMovieFee().times(percent);
     }
