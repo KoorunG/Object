be74fdfd7705eb4e21a7960b7102af9bf637b1fc
KoorunG <onlyzion10@naver.com>
2021-07-14 21:49:45 +0900

    플레이리스트 1차수정

--- songlist/PersonalPlayList.java
+++ songlist/PersonalPlayList.java
@@ -1,7 +1,8 @@
 package songlist;
 
-public class PersonalPlayList extends PlayList {
-    public void remove(Song song){
-        getTracks().remove(song);
+public class PersonalPlayList extends PlayList {    // PlayList를 상속받으며 
+    public void remove(Song song){                  // 노래를 제거하는 메소드 remove(Song)
+        getTracks().remove(song);                   // track -> List<Song> 이므로, remove 메소드 가능
+        getSingers().remove(song.getSinger());      // 
     }
 }
--- songlist/PlayList.java
+++ songlist/PlayList.java
@@ -1,16 +1,24 @@
 package songlist;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlayList {
     private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> singers = new HashMap<>();  // key : 가수이름, value : 노래제목
 
     public void append(Song song){
-        getTracks().add(song);
+        tracks.add(song);
+        singers.put(song.getSinger(),song.getTitle());
     }
 
     public List<Song> getTracks() {
         return tracks;
+    }
+
+    public Map<String,String> getSingers(){
+        return singers;
     }
 }
