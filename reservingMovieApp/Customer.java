34c4765bb214b0018971a83d95a28e86198ee625
KoorunG <onlyzion10@naver.com>
2021-07-14 14:43:41 +0900

    핸드폰 요금 계산

--- reservingMovieApp/Customer.java
+++ reservingMovieApp/Customer.java
@@ -3,11 +3,11 @@ package reservingMovieApp;
 import java.time.Duration;
 
 public class Customer {
-    public Money getAvatarFee(){
-        Movie avatar = new Movie("아바타",
-                                  Duration.ofMinutes(120)
-                                  Money.wons(10000),
-                                  new AmountDiscountPolicy()");
-        return avatar.getFee();
-    }
+    // public Money getAvatarFee(){
+    //     Movie avatar = new Movie("아바타",
+    //                               Duration.ofMinutes(120)
+    //                               Money.wons(10000),
+    //                               new AmountDiscountPolicy()");
+    //     return avatar.getFee();
+    // }
 }
